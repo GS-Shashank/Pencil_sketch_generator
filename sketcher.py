5607a2fee795fb822adee8559453a39493422fde
Shashank G S <69592231+GS-Shashank@users.noreply.github.com>
2021-06-05 16:55:21 +0530

    Add files via upload

---
+++ sketcher.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Project: Pencil Sketch Generator
+By: GSS
+"""
+
+import cv2 #importing the openCV library
+
+img=cv2.imread("input.jpeg");#importing the image
+
+#code to generate the sketch of the input image
+gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY);
+inv_gray_img=255-gray_img;
+blur_img=cv2.GaussianBlur(inv_gray_img,(21,21),0);
+inv_blur_img=255-blur_img;
+sketch=cv2.divide(gray_img,inv_blur_img,scale=240.0);
+
+#printing the sketch file
+cv2.imwrite("sketch.jpg",sketch);
